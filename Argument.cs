a89e036550ad06174709f517388d2b970e771927
unknown <OpenderS@wks-1280.intergen.org.nz>
2016-02-16 16:04:49 +1300

    Added EnumerableExtensions and Argument.cs classes.

---
+++ Argument.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CollectionExtensions
+{
+    /// <summary>
+    /// Represents a validator that simplifies checking method and constructor arguments and throws the 
+    /// appropriate exceptions.
+    /// </summary>
+    public static class Argument
+    {
+        [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = true)]
+        private sealed class ValidatedNotNullAttribute : Attribute { }
+
+        /// <summary>
+        /// Determine whether an argument is null and if it is, throw an <see cref="ArgumentNullException"/>.
+        /// </summary>
+        /// <param name="argument">The argument to be checked.</param>
+        /// <param name="argumentName">The name of the argument to be checked.</param>
+        /// <param name="argumentDescription">The description of the argument to be checked.</param>
+        public static void CheckIfNull([ValidatedNotNull] object argument, string argumentName, string argumentDescription)
+        {
+            VerifyArgumentNameIsNotNullOrEmpty(argumentName);
+            VerifyArgumentDescriptionIsNotNullOrEmpty(argumentDescription);
+
+            if (argument == null)
+            {
+                throw new ArgumentNullException(argumentName, argumentDescription + " cannot be null.");
+            }
+        }
+
+        /// <summary>
+        /// Checks if greater than zero.
+        /// </summary>
+        /// <param name="argument">The argument.</param>
+        /// <param name="argumentName">Name of the argument.</param>
+        public static void CheckIfGreaterThanZero([ValidatedNotNull]int argument, string argumentName)
+        {
+            if (argument <= 0)
+            {
+                throw new ArgumentException(string.Format("{0} cannot be less than 1. Value {1}.", argumentName, argument));
+            }
+        }
+
+        private static void VerifyArgumentDescriptionIsNotNullOrEmpty(string argumentDescription)
+        {
+            if (argumentDescription == null)
+            {
+                throw new ArgumentNullException("argumentDescription", "argumentDescription cannot be null.");
+            }
+            if (string.IsNullOrEmpty(argumentDescription))
+            {
+                throw new ArgumentException("argumentDescription cannot be empty.", "argumentDescription");
+            }
+        }
+
+        private static void VerifyArgumentNameIsNotNullOrEmpty(string argumentName)
+        {
+            if (argumentName == null)
+            {
+                throw new ArgumentNullException("argumentName", "argumentName cannot be null.");
+            }
+            if (string.IsNullOrEmpty(argumentName))
+            {
+                throw new ArgumentException("argumentName cannot be empty.", "argumentName");
+            }
+        }
+
+        /// <summary>
+        /// Determine whether an argument is null and if it is, throw an <see cref="ArgumentNullException"/>.
+        /// </summary>
+        /// <param name="argument">The argument to be checked.</param>
+        /// <param name="argumentName">The name of the argument to be checked.</param>
+        public static void CheckIfNull([ValidatedNotNull] object argument, string argumentName)
+        {
+            CheckIfNull(argument, argumentName, argumentName);
+        }
+
+        /// <summary>
+        /// Determine whether an argument is null or contains null and if it is, throw an <see cref="ArgumentNullException"/>.
+        /// </summary>
+        /// <param name="enumerable">The argument to be checked.</param>
+        /// <param name="argumentName">The name of the argument to be checked.</param>
+        public static void CheckIfNullOrContainsNull<T>([ValidatedNotNull] IEnumerable<T> enumerable, string argumentName)
+        {
+            CheckIfNull(enumerable, argumentName, argumentName);
+
+            foreach (var item in enumerable)
+            {
+                CheckIfNull(item, argumentName, argumentName);
+            }
+        }
+
+        /// <summary>
+        /// Determine whether a string argument is null or empty, throws an <see cref="ArgumentNullException"/> 
+        /// if it is null or an <see cref="ArgumentException"/> if it is empty.
+        /// </summary>
+        /// <param name="argument">The string argument to be checked.</param>
+        /// <param name="argumentName">The name of the string argument to be checked.</param>
+        /// <param name="argumentDescription">The description of the string argument to be checked.</param>
+        public static void CheckIfNullOrEmpty([ValidatedNotNull] string argument, string argumentName, string argumentDescription)
+        {
+            VerifyArgumentNameIsNotNullOrEmpty(argumentName);
+            VerifyArgumentDescriptionIsNotNullOrEmpty(argumentDescription);
+
+            if (argument == null)
+            {
+                throw new ArgumentNullException(argumentName, argumentDescription + " cannot be null.");
+            }
+            if (string.IsNullOrEmpty(argument))
+            {
+                throw new ArgumentException(argumentDescription + " cannot be empty.", argumentName);
+            }
+        }
+
+        /// <summary>
+        /// Determine whether a string argument is null or empty, throws an <see cref="ArgumentNullException"/> 
+        /// if it is null or an <see cref="ArgumentException"/> if it is empty.
+        /// </summary>
+        /// <param name="argument">The string argument to be checked.</param>
+        /// <param name="argumentName">The name of the string argument to be checked.</param>
+        public static void CheckIfNullOrEmpty([ValidatedNotNull]string argument, string argumentName)
+        {
+            CheckIfNullOrEmpty(argument, argumentName, argumentName);
+        }
+
+        /// <summary>
+        /// Determine whether a given database id is valid, i.e. at least 1.
+        /// </summary>
+        /// <param name="databaseId">The database id to be checked.</param>
+        /// <param name="argumentName">The name of the database id parameter to be checked.</param>
+        /// <param name="argumentDescription">The description of the database id parameter to be checked.</param>
+        /// <exception cref="ArgumentOutOfRangeException">if database id is less than 1.</exception>
+        public static void CheckDatabaseId([ValidatedNotNull] int databaseId, string argumentName, string argumentDescription)
+        {
+            VerifyArgumentNameIsNotNullOrEmpty(argumentName);
+            VerifyArgumentDescriptionIsNotNullOrEmpty(argumentDescription);
+
+
+            if (databaseId < 1)
+            {
+                throw new ArgumentOutOfRangeException(argumentName, databaseId,
+                    argumentDescription + " must be at least 1.");
+            }
+        }
+
+
+        /// <summary>
+        /// Determine whether a given database id is valid, i.e. at least 1.
+        /// </summary>
+        /// <param name="databaseId">The database id to be checked.</param>
+        /// <param name="databaseIdName">The name of the database id to be checked.</param>
+        /// <exception cref="ArgumentOutOfRangeException">if database id is less than 1.</exception>
+        public static void CheckDatabaseId([ValidatedNotNull] int databaseId, string databaseIdName)
+        {
+            CheckDatabaseId(databaseId, databaseIdName, databaseIdName);
+        }
+
+        /// <summary>
+        /// Checks if list is null or empty.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public static void CheckIfNullOrEmpty<T>([ValidatedNotNull] IEnumerable<T> enumerable, string argumentName)
+        {
+            VerifyArgumentNameIsNotNullOrEmpty(argumentName);
+
+            if (enumerable == null)
+            {
+                throw new ArgumentNullException(argumentName, "The list cannot be null.");
+            }
+            if (enumerable.Count() == 0)
+            {
+                throw new ArgumentException("The list cannot be empty.", argumentName);
+            }
+        }
+
+        /// <summary>
+        /// Checks the length of the string is less than or equal to the specified maxLength
+        /// </summary>
+        /// <param name="argument">The argument</param>
+        /// <param name="argumentName">The argument name.</param>
+        /// <param name="maxLength">Length of the max.</param>
+        public static void CheckStringNullOrLength([ValidatedNotNull]string argument, string argumentName, int maxLength)
+        {
+            CheckIfNull(argument, argumentName);
+            if (argument.Length > maxLength)
+            {
+                throw new ArgumentException(string.Format("The length of {0} exceeds {1} characters", argumentName, maxLength), argumentName);
+            }
+        }
+
+        /// <summary>
+        /// Checks the length of the argument is not empty and is less than or equal to the specified length.
+        /// </summary>
+        /// <param name="argument">The argument.</param>
+        /// <param name="argumentName">Name of the argument.</param>
+        /// <param name="maxLength">Length of the max.</param>
+        public static void CheckNullEmptyOrLength<T>([ValidatedNotNull] IEnumerable<T> argument, string argumentName, int maxLength)
+        {
+            CheckIfNullOrEmpty(argument, argumentName);
+
+            if (argument.Count() > maxLength)
+            {
+                throw new ArgumentException(string.Format("The length of {0} exceeds {1}", argumentName, maxLength), argumentName);
+            }
+        }
+
+        /// <summary>
+        /// Checks the length of the argument is not empty and is less than or equal to the specified length.
+        /// </summary>
+        /// <param name="argument">The argument.</param>
+        /// <param name="argumentName">Name of the argument.</param>
+        /// <param name="maxLength">Length of the max.</param>
+        public static void CheckNullEmptyOrLength([ValidatedNotNull] string argument, string argumentName, int maxLength)
+        {
+            CheckIfNullOrEmpty(argument, argumentName);
+            CheckStringNullOrLength(argument, argumentName, maxLength);
+        }
+    }
+}
---
+++ EnumerableExtensions.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionExtensions
+{
+    /// <summary>
+    /// Extension methods for Enumerable
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Determines if a collection is empty
+        /// </summary>
+        public static bool IsEmpty(this IEnumerable enumerable)
+        {
+            Argument.CheckIfNull(enumerable, "enumerable");
+
+            IEnumerator enumerator = enumerable.GetEnumerator();
+            return !enumerator.MoveNext();
+        }
+
+        /// <summary>
+        /// Simultaneiusly iterates two lists
+        /// </summary>
+        public static void IterateMultiple<T1, T2>(this IEnumerable<T1> first, IEnumerable<T2> second, Action<T1, T2> action)
+        {
+            Argument.CheckIfNull(action, "action");
+            Argument.CheckIfNull(first, "first");
+            Argument.CheckIfNull(second, "second");
+
+            using (var e1 = first.GetEnumerator())
+            using (var e2 = second.GetEnumerator())
+            {
+                while (e1.MoveNext() && e2.MoveNext())
+                {
+                    action(e1.Current, e2.Current);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Simultaneiusly iterates three lists
+        /// </summary>
+        public static void IterateMultiple<T1, T2, T3>(this IEnumerable<T1> first, IEnumerable<T2> second, IEnumerable<T3> third, Action<T1, T2, T3> action)
+        {
+            Argument.CheckIfNull(action, "action");
+            Argument.CheckIfNull(first, "first");
+            Argument.CheckIfNull(second, "second");
+            Argument.CheckIfNull(third, "third");
+
+            using (var e1 = first.GetEnumerator())
+            using (var e2 = second.GetEnumerator())
+            using (var e3 = third.GetEnumerator())
+            {
+                while (e1.MoveNext() && e2.MoveNext() && e3.MoveNext())
+                {
+                    action(e1.Current, e2.Current, e3.Current);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Simultaneiusly iterates four lists
+        /// </summary>
+        public static void IterateMultiple<T1, T2, T3, T4>(this IEnumerable<T1> first, IEnumerable<T2> second, IEnumerable<T3> third, IEnumerable<T4> fourth, Action<T1, T2, T3, T4> action)
+        {
+            Argument.CheckIfNull(action, "action");
+            Argument.CheckIfNull(first, "first");
+            Argument.CheckIfNull(second, "second");
+            Argument.CheckIfNull(third, "third");
+            Argument.CheckIfNull(fourth, "fourth");
+
+            using (var e1 = first.GetEnumerator())
+            using (var e2 = second.GetEnumerator())
+            using (var e3 = third.GetEnumerator())
+            using (var e4 = fourth.GetEnumerator())
+            {
+                while (e1.MoveNext() && e2.MoveNext() && e3.MoveNext() && e4.MoveNext())
+                {
+                    action(e1.Current, e2.Current, e3.Current, e4.Current);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Simultaneiusly iterates five lists
+        /// </summary>
+        public static void IterateMultiple<T1, T2, T3, T4, T5>(this IEnumerable<T1> first, IEnumerable<T2> second, IEnumerable<T3> third, IEnumerable<T4> fourth, IEnumerable<T5> fifth, Action<T1, T2, T3, T4, T5> action)
+        {
+            Argument.CheckIfNull(action, "action");
+            Argument.CheckIfNull(first, "first");
+            Argument.CheckIfNull(second, "second");
+            Argument.CheckIfNull(third, "third");
+            Argument.CheckIfNull(fourth, "fourth");
+            Argument.CheckIfNull(fifth, "fifth");
+
+            using (var e1 = first.GetEnumerator())
+            using (var e2 = second.GetEnumerator())
+            using (var e3 = third.GetEnumerator())
+            using (var e4 = fourth.GetEnumerator())
+            using (var e5 = fifth.GetEnumerator())
+            {
+                while (e1.MoveNext() && e2.MoveNext() && e3.MoveNext() && e4.MoveNext() && e5.MoveNext())
+                {
+                    action(e1.Current, e2.Current, e3.Current, e4.Current, e5.Current);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Iterates two lists and performs a select from both
+        /// </summary>
+        public static IEnumerable<TResult> IterateMultipleAndSelect<T1, T2, TResult>(this IEnumerable<T1> first, IEnumerable<T2> second, Func<T1, T2, TResult> selector)
+        {
+            Argument.CheckIfNull(selector, "selector");
+            Argument.CheckIfNull(first, "first");
+            Argument.CheckIfNull(second, "second");
+
+            var result = new List<TResult>();
+            first.IterateMultiple(second, (arg1, arg2) => result.Add(selector(arg1, arg2)));
+            return result;
+        }
+
+        /// <summary>
+        /// Iterates three lists and performs a select from all three
+        /// </summary>
+        public static IEnumerable<TResult> IterateMultipleAndSelect<T1, T2, T3, TResult>(this IEnumerable<T1> first, IEnumerable<T2> second, IEnumerable<T3> third, Func<T1, T2, T3, TResult> selector)
+        {
+            Argument.CheckIfNull(selector, "selector");
+            Argument.CheckIfNull(first, "first");
+            Argument.CheckIfNull(second, "second");
+            Argument.CheckIfNull(third, "third");
+
+            var result = new List<TResult>();
+            first.IterateMultiple(second, third, (arg1, arg2, arg3) => result.Add(selector(arg1, arg2, arg3)));
+            return result;
+        }
+
+        /// <summary>
+        /// Iterates four lists and performs a select from all four
+        /// </summary>
+        public static IEnumerable<TResult> IterateMultipleAndSelect<T1, T2, T3, T4, TResult>(this IEnumerable<T1> first, IEnumerable<T2> second, IEnumerable<T3> third, IEnumerable<T4> fourth, Func<T1, T2, T3, T4, TResult> selector)
+        {
+            Argument.CheckIfNull(selector, "selector");
+            Argument.CheckIfNull(first, "first");
+            Argument.CheckIfNull(second, "second");
+            Argument.CheckIfNull(third, "third");
+            Argument.CheckIfNull(fourth, "fourth");
+
+            var result = new List<TResult>();
+            first.IterateMultiple(second, third, fourth, (arg1, arg2, arg3, arg4) => result.Add(selector(arg1, arg2, arg3, arg4)));
+            return result;
+        }
+    }
+}
