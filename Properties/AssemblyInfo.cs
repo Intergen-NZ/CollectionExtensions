27278736902a0b700f83cf9dcff44b5bdd017613
Opender Singh <opender.94@gmail.com>
2016-02-16 16:34:35 +1300

    Updating Assembly Information for NuGet package and removing binaries.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CollectionExtensions")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Extensions for collections")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Intergen NZ")]
 [assembly: AssemblyProduct("CollectionExtensions")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
